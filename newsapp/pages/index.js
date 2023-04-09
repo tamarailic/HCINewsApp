23ba3d3fd269a2c68ddaa0009c1a151d3fefe4f0
Uros Pocek <uros.pocek@gmail.com>
2023-04-09 16:01:16 +0200

    feat: Setup for filters

--- newsapp/pages/index.js
+++ newsapp/pages/index.js
@@ -15,18 +15,18 @@ export default function Home() {
   const [filters, setFilters] = useState({ q: '', from: '', to: '', category: '', county: 'us' });
 
   return (<>
-            <Head>
-              <link rel="preconnect" href="https://fonts.googleapis.com"/>
-              <link rel="preconnect" href="https://fonts.gstatic.com"/>
-              <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet"/>
-            </Head>
-            <section className={styles.main_container} id='main'>
-              <LeftNav filters={filters} setFilters={setFilters} />
-              <TopNav filters={filters} setFilters={setFilters} />
-              <MainArea filters={filters} />
-            </section>
-          </>
-    
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet" />
+    </Head>
+    <section className={styles.main_container} id='main'>
+      <LeftNav filters={filters} setFilters={setFilters} />
+      <TopNav filters={filters} setFilters={setFilters} />
+      <MainArea filters={filters} />
+    </section>
+  </>
+
   )
 }
 
@@ -40,28 +40,28 @@ function LeftNav({ filters, setFilters }) {
 
 function TopNav({ filters, setFilters }) {
   return (
-          <div className={styles.nav_top}>
-            <div className={styles.navContents}>
-              <div>
-                <div>
-                  <Image src="/HCI_logo1.png" width={120} height={100} alt='News logo'></Image>
-                </div>
-                <div className={styles.currentDate}>{new Date(Date.now()).toDateString().slice(4,11)}</div>
-              </div>
-              <SearchBar/>
-            </div>
+    <div className={styles.nav_top}>
+      <div className={styles.navContents}>
+        <div>
+          <div>
+            <Image src="/HCI_logo1.png" width={120} height={100} alt='News logo'></Image>
           </div>
+          <div className={styles.currentDate}>{new Date(Date.now()).toDateString().slice(4, 11)}</div>
+        </div>
+        <SearchBar />
+      </div>
+    </div>
   );
 }
 
-function SearchBar(){
+function SearchBar() {
   return (
-  <div className={styles.search}>
-    <input type="text" name="search" className={styles.round} placeholder='Search' />
-    <div className={styles.imgDiv}>
-      <Image src="/images/searchIcon.png" width={24} height={24} alt='search icon'/>
-    </div>
-  </div>)
+    <div className={styles.search}>
+      <input type="text" name="search" className={styles.round} placeholder='Search' />
+      <div className={styles.imgDiv}>
+        <Image src="/images/searchIcon.png" width={24} height={24} alt='search icon' />
+      </div>
+    </div>)
 }
 function MainArea({ filters }) {
   const [newsToShow, setNewsToShow] = useState(null);
@@ -70,6 +70,8 @@ function MainArea({ filters }) {
   const { topNews, isLoading, isError } = getTopNews(currentPage);
 
   console.log(scrollPercentage);
+
+  hasAppliedFilters(filters);
 
   if (isLoading) return <Spinner />
   if (isError) return <Error />
@@ -82,7 +84,7 @@ function MainArea({ filters }) {
     <>
       {newsToShow !== null &&
         <div className={styles.main_area_container}>
-          <h2 className={styles.page_title}>Top Stories</h2>
+          <h2 className={styles.page_title}>{`${hasAppliedFilters(filters) ? 'For you' : 'Top Stories'}`}</h2>
           <div className={styles.story_grid}>
             {[newsToShow.shift()].map(bigStory => <BigStoryCard key={bigStory['title']} story={bigStory} />)}
             {newsToShow.map(regularStory => <RegularStoryCard key={regularStory['title']} story={regularStory} />)}
@@ -100,6 +102,16 @@ function getTopNews(page = 0, page_size = 20) {
     topNews: data,
     isLoading,
     isError: error
+  }
+}
+
+function hasAppliedFilters(filters) {
+  const filters_to_skip = ['county']
+  for (let filter in filters) {
+    if (filters_to_skip.includes(filter)) continue;
+    if (filters[filter]) {
+      return true;
+    }
   }
 }
 
