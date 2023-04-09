6b00c6c8c1bb4ab975851182369a26dee4c7f257
Uros Pocek <uros.pocek@gmail.com>
2023-04-09 19:49:37 +0200

    feat: Added paggination for news

---
+++ newsapp/pages/articles/[id].js
@@ -0,0 +1,7 @@
+export default function Article() {
+    return (
+        <div>
+            h1
+        </div>
+    );
+}
--- newsapp/pages/index.js
+++ newsapp/pages/index.js
@@ -1,15 +1,14 @@
 import styles from '@/styles/Home.module.css'
 import useSWR from 'swr'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Spinner from '@/components/spinner';
 import Error from '@/components/error';
-import useScrollPercentage from '@/components/useScrollProcentage';
 import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
-const newsApiKey = '339e56b6d42f4a0ab9c4f820c0a35558';
+const newsApiKey = 'f0885235e81b407294d7c96b9e3f60a4';
 
 export default function Home() {
   const [filters, setFilters] = useState({ q: '', from: '', to: '', category: '', county: 'us' });
@@ -71,13 +70,45 @@ function SearchBar() {
 }
 function MainArea({ filters }) {
   const [newsToShow, setNewsToShow] = useState(null);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [scrollRef, scrollPercentage] = useScrollPercentage();
-  const { topNews, isLoading, isError } = getTopNews(currentPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [readyToLoadMore, setReadyToLoadMore] = useState(true);
 
-  console.log(scrollPercentage);
+  const getNews = hasAppliedFilters(filters) ? getFilteredNews : getTopNews;
+  const { topNews, isLoading, isError } = getNews(filters, currentPage);
 
-  hasAppliedFilters(filters);
+  useEffect(() => {
+    const procentageOfScrollAfterToLoadNewStories = 90;
+    const handleScroll = async () => {
+      const scrollPercentage = getScrollPercent();
+      if (readyToLoadMore && currentPage <= 4 && scrollPercentage > procentageOfScrollAfterToLoadNewStories) {
+        setReadyToLoadMore(false);
+        setCurrentPage(currentPage + 1);
+        const loadMoreNews = hasAppliedFilters(filters) ? loadMoreFilteredNews : loadMoreTopNews;
+        const newStories = await loadMoreNews(currentPage + 1);
+
+        if (newStories != null) {
+          const allNews = JSON.parse(JSON.stringify(newsToShow)).concat(newStories['articles']);
+          setNewsToShow(allNews.filter(item => item != undefined));
+          if (!allNews.includes(undefined)) {
+            setReadyToLoadMore(true);
+          }
+        }
+      }
+    }
+    function getScrollPercent() {
+      var h = document.documentElement,
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+      return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [newsToShow, currentPage, readyToLoadMore]);
 
   if (isLoading) return <Spinner />
   if (isError) return <Error />
@@ -92,8 +123,8 @@ function MainArea({ filters }) {
         <div className={styles.main_area_container}>
           <h2 className={styles.page_title}>{`${hasAppliedFilters(filters) ? 'For you' : 'Top Stories'}`}</h2>
           <div className={styles.story_grid}>
-            {[newsToShow.shift()].map(bigStory => <BigStoryCard key={bigStory['title']} story={bigStory} />)}
-            {newsToShow.map(regularStory => <RegularStoryCard key={regularStory['title']} story={regularStory} />)}
+            {[newsToShow[0]].map(bigStory => <BigStoryCard key={bigStory['title'] || crypto.randomUUID()} story={bigStory} />)}
+            {newsToShow.slice(1).map(regularStory => <RegularStoryCard key={regularStory['title'] || crypto.randomUUID()} story={regularStory} />)}
           </div>
         </div>
       }
@@ -101,14 +132,47 @@ function MainArea({ filters }) {
   );
 }
 
-function getTopNews(page = 0, page_size = 20) {
-  const { data, error, isLoading } = useSWR(`https://newsapi.org/v2/top-headlines?language=en&pageSize=${page_size}&page=${page}&apiKey=${newsApiKey}`, fetcher);
+function getTopNews(filters, page = 1, page_size = 20) {
+  const { data, error, isLoading } = useSWR(`https://newsapi.org/v2/top-headlines?language=en&country=${filters['county']}&pageSize=${page_size}&page=${page}&apiKey=${newsApiKey}`, fetcher);
 
   return {
     topNews: data,
     isLoading,
     isError: error
   }
+}
+
+function getFilteredNews(filters, page = 1, page_size = 20) {
+  let url = ` https://newsapi.org/v2/everything?language=en&country=${filters['county']}&pageSize=${page_size}&page=${page}`;
+  if (filters['q']) url += `&q=${filters['q']}`;
+  if (filters['from']) url += `&from=${filters['from']}`;
+  if (filters['to']) url += `&to=${filters['to']}`;
+  if (filters['category']) url += `&category=${filters['category']}`;
+  url += `&apiKey=${newsApiKey}`;
+
+  const { data, error, isLoading } = useSWR(url, fetcher);
+
+  return {
+    topNews: data,
+    isLoading,
+    isError: error
+  }
+}
+
+async function loadMoreTopNews(page = 0, page_size = 20) {
+  const moreNews = await fetch(`https://newsapi.org/v2/top-headlines?language=en&pageSize=${page_size}&page=${page}&apiKey=${newsApiKey}`);
+  return await moreNews.json();
+}
+
+async function loadMoreFilteredNews(page = 0, page_size = 20) {
+  let url = ` https://newsapi.org/v2/everything?language=en&country=${filters['county']}&pageSize=${page_size}&page=${page}`;
+  if (filters['q']) url += `&q=${filters['q']}`;
+  if (filters['from']) url += `&from=${filters['from']}`;
+  if (filters['to']) url += `&to=${filters['to']}`;
+  if (filters['category']) url += `&category=${filters['category']}`;
+  url += `&apiKey=${newsApiKey}`;
+  const moreNews = await fetch(url);
+  return await moreNews.json();
 }
 
 function hasAppliedFilters(filters) {
@@ -123,7 +187,7 @@ function hasAppliedFilters(filters) {
 
 function RegularStoryCard({ story }) {
   return (
-    <Link href={story['url']} style={{ textDecoration: 'none' }}>
+    <Link href={`/articles/${story['title']}`} style={{ textDecoration: 'none' }}>
       <div className={styles.regular_story + ' ' + styles.card}>
         <div className={styles.main_story_area}>
           <div className={styles.story_content}>
@@ -131,7 +195,7 @@ function RegularStoryCard({ story }) {
             <div className={styles.story_title}>{`${story['title'].split(' ').slice(0, 12).join(' ')}`}</div>
           </div>
           <div className={styles.story_image}>
-            <Image src={story['urlToImage']} alt={story['title']} width={130} height={130} />
+            {story['urlToImage'] ? <Image src={story['urlToImage']} alt={story['title']} width={130} height={130} /> : <Image src="/images/articleImageBackup.jpg" alt={story['title']} width={130} height={130} />}
           </div>
         </div>
         <div className={styles.story_footer}>
@@ -140,6 +204,28 @@ function RegularStoryCard({ story }) {
         </div>
       </div>
     </Link>
+  );
+}
+
+function BigStoryCard({ story }) {
+  return (
+    <div className={styles.big_story + ' ' + styles.card}>
+      <Link href={`/articles/${story['title']}`} style={{ textDecoration: 'none' }}>
+        <div className={styles.main_story_area}>
+          <div className={styles.story_image}>
+            {story['urlToImage'] ? <img src={story['urlToImage']} alt={story['title']} /> : <img src="/images/articleImageBackup.jpg" alt={story['title']} />}
+          </div>
+          <div className={styles.story_content}>
+            <div className={styles.story_source}>{story['source']['name'] || ''}</div>
+            <div className={styles.story_title}>{story['title']}</div>
+          </div>
+        </div>
+      </Link>
+      <div className={styles.story_footer}>
+        <p>{`${calculatePublishTime(story['publishedAt'])} • ${story['author'] || 'anonymous'}`}</p>
+        <p className={styles.read_article_btn}>{'➤'}</p>
+      </div>
+    </div>
   );
 }
 
@@ -156,7 +242,7 @@ function calculatePublishTime(publishedDate) {
   if (month != now.getMonth() + 1) {
     return +now.getMonth() + 1 - +month + 'month(s) ago';
   }
-  if (day != now.getDate()) {
+  if (+day.split('0')[1] + 1 != now.getDate()) {
     return +now.getDate() - +day + 'day(s) ago';
   }
   if (hours != now.getHours()) {
@@ -168,25 +254,4 @@ function calculatePublishTime(publishedDate) {
   if (seconds != now.getSeconds()) {
     return +now.getSeconds() - +seconds + 'second(s) ago';
   }
-}
-
-function BigStoryCard({ story }) {
-  return (
-    <div className={styles.big_story + ' ' + styles.card}>
-      <div className={styles.main_story_area}>
-        <div className={styles.story_image}>
-          <img src={story['urlToImage']} alt={story['title']} />
-        </div>
-        <div className={styles.story_content}>
-          <div className={styles.story_source}>{story['source']['name'] || ''}</div>
-          <div className={styles.story_title}>{story['title']}</div>
-        </div>
-      </div>
-      <div className={styles.story_footer}>
-        <p>{`${calculatePublishTime(story['publishedAt'])} • ${story['author'] || 'anonymous'}`}</p>
-        <p className={styles.read_article_btn}>{'➤'}</p>
-      </div>
-
-    </div>
-  );
 }
